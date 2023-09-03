dcbde0667d38858d58615e81eeb27aeb90c051cd
AKCodez <ariakesh@yahoo.com>
2023-09-02 22:21:25 -0700

    updater try cathc

--- kraken.js
+++ kraken.js
@@ -3,22 +3,7 @@ require('dotenv').config() // 0 for unlimited or a specific number higher than y
 const puppeteer = require('puppeteer');
 const proxyChain = require('proxy-chain');
 const proxyArr = require('./output.json')
-function generateRandomText(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
 
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-    }
-
-    return result;
-}
-function generateRandomTimeout() {
-    // Generates a random number between 1 and 8 (inclusive) and multiplies it by 1000 to convert it to milliseconds
-    return Math.floor(Math.random() * 8 + 1) * 1000;
-}
- 
 function generateUserAgent() {
     const platform = ['Windows NT 10.0', 'Macintosh; Intel Mac OS X 10_15_7', 'X11; Linux x86_64'];
     const chromeVersion = ['Chrome/90', 'Chrome/89', 'Chrome/88', 'Chrome/87', 'Chrome/86'];
@@ -33,6 +18,7 @@ let count = 0
 let currentIndex = Number(process.env.start_index);
 let endIndex = Number(process.env.end_index);
 const ReleaseTheKraken = async () => {
+    try {
     console.log({currentIndex})
     if (currentIndex >= endIndex) {
         console.log('All proxies have been used.');
@@ -40,7 +26,9 @@ const ReleaseTheKraken = async () => {
         return;
     }
     const proxy = proxyArr[currentIndex];
+    console.log('annoning proxy')
     const newProxyUrl = await proxyChain.anonymizeProxy(`http://${proxy}`);
+    console.log('launching browser')
     const browser = await puppeteer.launch({
         headless: "new",
         args: [`--proxy-server=${newProxyUrl}`, '--no-sandbox', '--disable-setuid-sandbox'],
@@ -50,28 +38,13 @@ const ReleaseTheKraken = async () => {
     const page = await browser.newPage();
     const userAgent = generateUserAgent()
     await page.setUserAgent(userAgent); // Using the generated User-Agent here
-    try {
         await page.goto('https://www.twitch.tv/thiccsnorlex', { timeout: 60000 }); // Increasing timeout to 60 seconds
-        // const randomTimeout = generateRandomTimeout();
-        // console.log(`Random timeout generated: ${randomTimeout} milliseconds`); // Output will be a random number between 1000 and 8000
-        // const timeout = setTimeout(async () => {
-        //     const element = await page.$('input[type="search"]');
-        //     if (element) {
-        //         const boundingBox = await element.boundingBox();
-        //         const x = boundingBox.x + boundingBox.width / 2;
-        //         const y = boundingBox.y + boundingBox.height / 2;
-        //         await page.mouse.move(x, y);
-        //         await page.mouse.click(x, y);
-        //     }
-        //     const randomText = generateRandomText(8);
-        //     await element.type(randomText); // Replace with your CSS selector and text
-        // clearTimeout(timeout)
-        // },randomTimeout)
         console.log(`#${count} BROWSER WATCHING STREAM.... MOVING TO NEXT BROWSER \n\n`);
     } catch (error) {
         console.error(error);
         count--
         console.log(`Navigation FAILED with proxy: ${proxy}. Skipping to the next proxy. reducing count to ${count}`);
+        
     }
     currentIndex++;
     count++
